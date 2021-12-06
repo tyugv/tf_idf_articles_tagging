313c43bdf06ffa678af8def9ad397e5b0dd72fcb
milena <mso>
2021-12-06 13:10:27 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name="articles_tagging",
       url='https://github.com/tyugv/tf_idf_articles_tagging',
       install_requires=["joblib==1.1.0",
                         "nltk==3.6.5",
-                        "pandas==1.3.4",
+                        "pandas",
                         "scikit-learn==1.0.1"]
       )
