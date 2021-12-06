94bf4caba215c159ae1ddeb04ff501b2eb9a86a3
milena <mso>
2021-12-06 13:20:17 +0300

    load tf_idf model

--- src/example.py
+++ src/example.py
@@ -1,17 +1,12 @@
 import os
 import sys
-from joblib import load
 
 from articles_tagging.tagging import tagging
 from articles_tagging.training import training
 
 
 if __name__ == '__main__':
-    if 'tf_idf.joblib' not in os.listdir('model_weight'):
-        training()
-    tf_idf = load('model_weight/tf_idf.joblib')
-
     if len(sys.argv) > 1:
-        print(tagging(str(sys.argv[1]), tf_idf))
+        print(tagging(str(sys.argv[1])))
     else:
-        print(tagging('Hello world!', tf_idf))
+        print(tagging('Hello world!'))
--- src/tagging.py
+++ src/tagging.py
@@ -4,7 +4,10 @@ from joblib import load
 from articles_tagging.preprocessing import preprocess
 
 
-def tagging(text, tf_idf_model, max_value=0.15):
+def tagging(text, max_value=0.15):
+    if 'tf_idf.joblib' not in os.listdir('model_weight'):
+        training()
+    tf_idf_model = load('model_weight/tf_idf.joblib')
     text = preprocess(text)
     result = tf_idf_model.transform([text]).toarray()[0]
     tags = []
