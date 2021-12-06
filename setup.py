ee082a4d85717259563d0b1a094c0785a04ab3fc
milena <mso>
2021-12-06 11:36:54 +0300

    packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name="articles_tagging",
+      version="0.0.1",
+      description='file: README.md',
+      author='Milena',
+      url='https://github.com/tyugv/tf_idf_articles_tagging',
+      )
+
+_deps = ["joblib==1.1.0",
+         "nltk==3.6.5",
+         "pandas==1.3.4",
+         "scikit-learn==1.0.1"]
