643a95c33405b06461c7f72a4f08ec9be3044b05
milena <mso>
2021-12-06 11:39:06 +0300

    add example

---
+++ src/example.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+from tagging import tagging
+from training import training
+
+
+if __name__ == '__main__':
+    if 'tf_idf.joblib' not in os.listdir('../model_weight'):
+        training()
+    tf_idf = load('../model_weight/tf_idf.joblib')
+
+    if len(sys.argv) > 1:
+        print(tagging(str(sys.argv[1]), tf_idf))
+    else:
+        print(tagging('Hello world!', tf_idf))
---
+++ src/tagging.py
@@ -0,0 +1,14 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from joblib import load
+
+from preprocessing import preprocess
+
+
+def tagging(text, tf_idf_model, max_value=0.15):
+    text = preprocess(text)
+    result = tf_idf_model.transform([text]).toarray()[0]
+    tags = []
+    for value, word in zip(result, tf_idf_model.get_feature_names_out()):
+        if value > max_value:
+            tags.append(word)
+    return tags
