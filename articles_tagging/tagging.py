70319116d4fa69c24badfe4a169042ee09fc2925
milena <mso>
2021-12-20 12:39:27 +0300

    src to articles_tagging and sort tags

--- articles_tagging/tagging.py
+++ articles_tagging/tagging.py
@@ -1,5 +1,4 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-
 from articles_tagging.preprocessing import preprocess
 
 
@@ -7,7 +6,10 @@ def tagging(text, tf_idf_model, max_value=0.15):
     text = preprocess(text)
     result = tf_idf_model.transform([text]).toarray()[0]
     tags = []
+    values = []
     for value, word in zip(result, tf_idf_model.get_feature_names_out()):
         if value > max_value:
             tags.append(word)
-    return tags
+            values.append(value)
+    sorted_tags = [tags[i[0]] for i in sorted(enumerate(values), key=lambda x:x[1], reverse=True)]
+    return sorted_tags
